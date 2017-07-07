8353691b784f429833a26316872150fe31e0219c
rordev <rordev@ongraph.com>
2017-07-07 12:48:24 +0530

    controller bug fixed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
 
-  before_filter :authenticate_customer!
+  before_action :authenticate_customer!
   def profile
     @customer = current_customer
   end
