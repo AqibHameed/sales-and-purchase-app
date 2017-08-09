5e79c3380bec8230366c43d6268e844971e5bd4f
shankar <shankar.yuvasoft15@gmail.com>
2017-08-09 18:22:51 +0530

    Add auction module

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :auctions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login', :to => 'home#login'
   get '/signup', :to => 'home#registration'
