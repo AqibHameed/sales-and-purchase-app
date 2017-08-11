28b96581e123483ba7c03b18213ee1020613f59c
shankar <shankar.yuvasoft15@gmail.com>
2017-08-11 18:48:28 +0530

    Design add in auction

--- db/migrate/20170809121845_create_auctions.rb
+++ db/migrate/20170809121845_create_auctions.rb
@@ -6,7 +6,8 @@ class CreateAuctions < ActiveRecord::Migration[5.1]
       t.integer :tender_id
       t.integer :round_time
       t.boolean :started, default: false
-
+      t.boolean :completed, default: false
+      t.integer :loosers_per_round
       t.timestamps
     end
   end
