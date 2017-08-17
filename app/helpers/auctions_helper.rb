e4dd3d55cf78475f3a93f876ee4bdf24c203ca10
shankar <shankar.yuvasoft15@gmail.com>
2017-08-17 15:31:52 +0530

    Update page design

--- app/helpers/auctions_helper.rb
+++ app/helpers/auctions_helper.rb
@@ -1,10 +1,14 @@
 module AuctionsHelper
 
   def can_user_place_bid? stone
-     @auction.round_loosers.where(stone_id: stone.id, customer_id: current_customer.id).blank? rescue true
+    @auction.round_loosers.where(stone_id: stone.id, customer_id: current_customer.id).blank? rescue true
   end
 
-  def user_win_last_round_for_stone? stone
+  def user_loose_auction_for_stone? stone
+    @auction.round_loosers.where(stone_id: stone.id, customer_id: current_customer.id).present?
+  end
+
+  def user_win_auction_for_stone? stone
   	@auction.round_winners.where(stone_id: stone.id, customer_id: current_customer.id).present?
   end	
 end
