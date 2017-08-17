c0dcc891497493042fd7612a26c2bfdd9a36641e
shankar <shankar.yuvasoft15@gmail.com>
2017-08-17 10:13:47 +0530

    show winner after auction

--- app/helpers/auctions_helper.rb
+++ app/helpers/auctions_helper.rb
@@ -3,4 +3,8 @@ module AuctionsHelper
   def can_user_place_bid? stone
      @last_round.present? ? @auction.round_loosers.where(stone_id: stone.id, customer_id: current_customer.id).blank? : true
   end
+
+  def user_win_last_round_for_stone? stone
+  	@auction.round_winners.where(stone_id: stone.id, customer_id: current_customer.id).present?
+  end	
 end
