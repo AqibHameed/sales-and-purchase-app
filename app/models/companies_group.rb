c9dde85c974f9b0a9688666567c163a71de83286
rordev <rordev@ongraph.com>
2018-06-01 19:32:16 +0530

    validation added

--- app/models/companies_group.rb
+++ app/models/companies_group.rb
@@ -1,6 +1,8 @@
 class CompaniesGroup < ApplicationRecord
   serialize :customer_id, Array
 
+  validates :group_name, :customer_id, presence: true
+
   belongs_to :seller, class_name: 'Customer', foreign_key: 'seller_id'
   belongs_to :companies_customer, class_name: 'Customer', foreign_key: 'customer_id', optional: true
 end
