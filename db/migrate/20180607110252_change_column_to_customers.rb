5ddd6f6edbe636764b3d223383c13e538f8f4209
rordev <rordev@ongraph.com>
2018-06-11 15:02:12 +0530

    updated migration

--- db/migrate/20180607110252_change_column_to_customers.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnToCustomers < ActiveRecord::Migration[5.1]
-  def change
-  	rename_column :customers, :company, :company_id
-  	change_column :customers, :company_id, :integer
-  end
-end
