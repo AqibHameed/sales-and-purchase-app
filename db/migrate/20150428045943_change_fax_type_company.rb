5275dd6bf0e2a950ad105608fc4028c2e4808287
rordev <rordev@ongraph.com>
2018-07-04 12:22:22 +0530

    remove comapny related migrations

--- db/migrate/20150428045943_change_fax_type_company.rb
+++
@@ -1,9 +0,0 @@
-class ChangeFaxTypeCompany < ActiveRecord::Migration[5.1]
-  def up
-  		change_column :companies, :fax, :string if column_exists?(:companies, :fax, :integer)
-  	  change_column :companies, :telephone, :string if column_exists?(:companies, :telephone, :integer)
-  end
-
-  def down
-  end
-end
