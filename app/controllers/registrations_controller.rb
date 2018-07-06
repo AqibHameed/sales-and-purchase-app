f2f98c93040ccf3091eff8a2d77f5c8b85a705fd
rordev <rordev@ongraph.com>
2018-07-06 16:37:40 +0530

    updated ragistration page for broker

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,19 +3,25 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     company = Company.where(name: sign_up_params[:company_name]).first_or_create
     build_resource(sign_up_params.merge(company_id: company.id))
-    resource.save
+    if sign_up_params[:role] == 'Broker'
+      company.try(:customers).present? ? resource.errors.add(:company, 'Already exists...') : ''
+    end
+    resource.save unless resource.errors.present?
     yield resource if block_given?
-    if resource.persisted?
+    if resource.persisted? && !resource.errors.present?
       if resource.active_for_authentication?
+
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
       else
+
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
         respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
+
       company.destroy unless company.try(:customers).present?
       clean_up_passwords resource
       set_minimum_password_length
@@ -24,7 +30,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   private
-  
+
   def company_params
     params.permit!
   end
@@ -36,3 +42,4 @@ class RegistrationsController < Devise::RegistrationsController
     trading_customers_path
   end
 end
+
