2c7759a7eb63e89aae7c88ff373bd958a45203e6
rordev <rordev@ongraph.com>
2018-07-09 15:42:12 +0530

    demand email

--- app/models/trading_parcel.rb
+++ app/models/trading_parcel.rb
@@ -69,8 +69,10 @@ class TradingParcel < ApplicationRecord
   end
 
   def send_mail_to_demanded
-    demands = Demand.where(description: self.description, demand_supplier_id: self.customer_id).map { |e| e.customer.email }
-    TenderMailer.parcel_up_email(self, demands).deliver rescue logger.info "Error sending email"
+    demands = Demand.where.not(company_id: company_id).where(description: self.description).map { |e| e.customer.email }
+    unless demands.blank?
+      TenderMailer.parcel_up_email(self, demands).deliver rescue logger.info "Error sending email"
+    end
   end
 
   def self.send_won_parcel_email(proposal)
