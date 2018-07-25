3b5d9f6560ccd10f12049f20fd16e942590f49a5
rordev <rordev@ongraph.com>
2018-07-25 18:48:27 +0530

    validation added for polished demand

--- app/models/polished_demand.rb
+++ app/models/polished_demand.rb
@@ -2,7 +2,8 @@ class PolishedDemand < ApplicationRecord
   belongs_to :company
   belongs_to :demand_supplier
   default_scope { where(deleted: false) }
-  
+  validates :shape, :weight_from, :weight_to, :color_from, :color_to, :clarity_from, :clarity_to, :cut_from, :cut_to, :polish_from, :polish_to, :symmetry_from, :symmetry_to, :fluorescence_from, :fluorescence_to, :lab, presence: true
+
   def self.update_polished_demands_block_unblock
     PolishedDemand.all.each do |d|
       company = d.company
