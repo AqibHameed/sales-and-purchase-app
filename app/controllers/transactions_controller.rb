5b21221e8390a760dec95282ddfa0f3d888f46b9
rordev <rordev@ongraph.com>
2018-07-31 11:59:06 +0530

    paid is removed from view invoice page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -116,6 +116,6 @@ class TransactionsController < ApplicationController
   end
 
   def update_transaction_params
-    params.require(:transaction).permit(:paid, :total_amount, :price, :invoice_no, :ready_for_buyer, :created_at)
+    params.require(:transaction).permit(:total_amount, :price, :invoice_no, :ready_for_buyer, :created_at)
   end
 end
