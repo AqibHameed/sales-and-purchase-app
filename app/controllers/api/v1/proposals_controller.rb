7d90b72feee218c995b0470f2115bdf98dcb5f78
rordev <rordev@ongraph.com>
2018-08-06 15:58:25 +0530

    proposal api updated

--- app/controllers/api/v1/proposals_controller.rb
+++ app/controllers/api/v1/proposals_controller.rb
@@ -5,11 +5,12 @@ module Api
 
       def create
         if current_company
-          parcel = TradingParcel.where(params[:trading_parcel_id]).first
+          parcel = TradingParcel.where(id: params[:trading_parcel_id]).first
           proposal = Proposal.new(proposal_params)
           proposal.buyer_id = current_company.id
           proposal.seller_id = parcel.company_id
           if proposal.save
+            Message.create_new(proposal)
             render json: { success: true, message: 'Proposal Submitted Successfully' }
           else
             render json: { success: false, errors: proposal.errors.full_messages }
