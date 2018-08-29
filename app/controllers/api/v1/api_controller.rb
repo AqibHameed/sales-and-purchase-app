2944f519af71aeb956c38ba58af3f1c72f32bc45
rordev <rordev@ongraph.com>
2018-08-29 17:25:28 +0530

    supplier connected in api controller updated

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -183,7 +183,7 @@ class Api::V1::ApiController < ApplicationController
         created_at: c.created_at,
         updated_at: c.updated_at,
         purchases_completed: get_completed_transaction(c),
-        suppliers_connected: supplier_connected(c, current_company),
+        suppliers_connected: supplier_connected(c.id),
         status: status
       }
     end
@@ -193,4 +193,9 @@ class Api::V1::ApiController < ApplicationController
   def attachment_params
     params.require(:email_attachment).permit(:file,:tender_id)
   end
+
+  def supplier_connected(buyer)
+    count = CreditLimit.where("buyer_id =?", buyer).count
+  end
+
 end
