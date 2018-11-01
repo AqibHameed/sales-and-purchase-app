5203c97e736ddfd65073ef1b6a33552fc8303443
rordev <rordev@ongraph.com>
2018-11-01 18:58:41 +0530

    direct sell proposal updated

--- app/controllers/api/v1/trading_parcels_controller.rb
+++ app/controllers/api/v1/trading_parcels_controller.rb
@@ -193,12 +193,12 @@ module Api
             if credit_limit.nil?
               credit_limit = CreditLimit.create(buyer_id: transaction.buyer_id, seller_id: current_company.id, credit_limit: total_price)
             else
-              new_limit = credit_limit.credit_limit + (total_price - available_limit)
+              new_limit = credit_limit.credit_limit.to_f + (total_price.to_f - available_limit.to_f)
               credit_limit.update_attributes(credit_limit: new_limit)
             end
           end
           if available_limit < total_price
-            new_market_limit =  credit_limit.market_limit + total_price - available_limit
+            new_market_limit =  credit_limit.market_limit.to_f + total_price.to_f - available_limit.to_f
             credit_limit.update_attributes(market_limit: new_market_limit)
           end
           parcel.update_attributes(sold: true)
