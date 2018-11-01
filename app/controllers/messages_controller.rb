1cef10ad7e723c6517607b5bac5c27b2fbbaddda
rordev <rordev@ongraph.com>
2018-11-01 19:18:54 +0530

    message sorting by time added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
 
   def index
     @messages = []
-    all_messages = Message.joins(:sender).order("companies.name").where(receiver_id: current_company.id)
+    all_messages = Message.joins(:sender).order("created_at").where(receiver_id: current_company.id)
     all_messages.group_by(&:proposal_id).each do |proposal_id, messages|
       @messages << messages.last
     end
