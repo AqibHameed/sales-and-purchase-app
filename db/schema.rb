ab4d6645c18fcfd1cc8d20ecf312cccba983e003
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-11-17 02:10:39 +0500

    add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101105030) do
+ActiveRecord::Schema.define(version: 20181116192513) do
 
   create_table "admins", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string "email", default: "", null: false
@@ -558,7 +558,7 @@ ActiveRecord::Schema.define(version: 20181101105030) do
     t.decimal "price", precision: 12, scale: 2
     t.integer "credit"
     t.text "notes"
-    t.integer "status", default: 0
+    t.integer "status", default: 4
     t.integer "action_for"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
