80a63f8c3fdf62d0f21ea5e725f42093697fd517
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-11-20 15:49:30 +0500

    add validation for market limit and credit limit.

--- app/models/credit_limit.rb
+++ app/models/credit_limit.rb
@@ -1,14 +1,16 @@
 class CreditLimit < ApplicationRecord
   include LiveMonitor
 
-  validates_numericality_of :credit_limit
+  validates_numericality_of :credit_limit, greater_than_or_equal_to: 0
+  validates_numericality_of :market_limit, greater_than_or_equal_to: 0
+  validates_presence_of :market_limit, :credit_limit
   after_create :secure_center
   after_update :secure_center
   belongs_to :buyer, class_name: 'Company', foreign_key: 'buyer_id'
   belongs_to :seller, class_name: 'Company', foreign_key: 'seller_id'
-  validate :verify_market_limit_is_greater_than_credit_limit?
+  validate :verify_market_limit_is_greater_than_or_equal_to_credit_limit?
 
-  def verify_market_limit_is_greater_than_credit_limit?
+  def verify_market_limit_is_greater_than_or_equal_to_credit_limit?
     errors.add(:market_limit, 'Should be greater than credit limit') if credit_limit > market_limit
   end
 
