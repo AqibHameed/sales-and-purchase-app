5182deb27e87e4f14de0ab60d3b4cf02d6c6bc1d
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-11-24 22:26:29 +0500

    merge limits api

--- app/controllers/api/v1/limits_controller.rb
+++ app/controllers/api/v1/limits_controller.rb
@@ -3,69 +3,24 @@ module Api
     class LimitsController < ApiController
       before_action :current_customer
       skip_before_action :verify_authenticity_token, only: [:add_credit_limit, :add_market_limit, :add_overdue_limit, :block, :unblock, :credit_limit_list]
-
+      protect_from_forgery with: :null_session
       include ActionView::Helpers::NumberHelper
       include ActionView::Helpers::TextHelper
       include ApplicationHelper
 
-      def add_credit_limit
+      def add_limits
         if current_company
-          cl = CreditLimit.where(seller_id: current_company.id, buyer_id: params[:buyer_id]).first_or_initialize
-          # total_clms = CreditLimit.where(seller_id: current_company.id).sum(:credit_limit)
-          cl.credit_limit = params[:limit].to_f
-          cl.errors.add(:credit_limit, "should not be negative ") if params[:limit].to_f < 0
-
-          # if current_customer.parent_id.present?
-          #   sub_company_limit = SubCompanyCreditLimit.find_by(sub_company_id: current_company.id)
-          #   if sub_company_limit.try(:credit_type) == "General"
-          #     limit = sub_company_limit.credit_limit
-          #   elsif sub_company_limit.try(:credit_type) == "Specific"
-          #     limit = cl.credit_limit
-          #     unless limit.present?
-          #       cl.errors.add(:credit_limit, "not set by parent company")
-          #     end
-          #   else
-          #     cl.errors.add(:credit_limit, "not set by parent company")
-          #   end
-          #   if limit.to_f < (total_limit.to_f + total_clms.to_f)
-          #     cl.errors.add(:credit_limit, "can't be greater than assigned limit")
-          #   else
-          #     cl.credit_limit  = total_limit.to_f
-          #   end
-          # else
-          #   cl.credit_limit  = total_limit.to_f
-          # end
-          if cl.errors.any?
-            render json: { success: false, errors: cl.errors.full_messages.first }
+          buyer_limits = CreditLimit.find_by(buyer_id: params[:limit]['buyer_id'], seller_id: current_company.id)
+          buyer_limits = CreditLimit.new(seller_id: current_company.id, buyer_id: params[:limit]['buyer_id']) if buyer_limits.nil?
+          buyer_limits.credit_limit = params[:limit]['credit_limit'] unless params[:limit]['credit_limit'].blank?
+          buyer_limits.market_limit = params[:limit]['market_limit'] unless params[:limit]['market_limit'].blank?
+          if buyer_limits.save
+            render json: {success: true, message: 'Limits updated.'}
           else
-            if cl.save
-              render json: { success: true, message: 'Credit Limit updated.', limit: cl.credit_limit }
-            else
-              render json: { success: false, message: cl.errors.full_messages.first }
-            end
+            render json: {success: false, message: buyer_limits.errors.full_messages}
           end
         else
-          render json: { errors: "Not authenticated", response_code: 201 }
-        end
-      end
-
-      def add_market_limit
-        if current_company
-          buyer = Company.where(id: params[:buyer_id]).first
-          if buyer.nil?
-            render json: { success: false, message: "Buyer doesn't exist" }
-          else
-            cl = CreditLimit.where(buyer_id: params[:buyer_id], seller_id: current_company.id).first_or_initialize
-            cl.market_limit = params[:limit]
-            cl.credit_limit = 0 if cl.credit_limit.nil?
-            if cl.save!
-              render json: { success: true, message: 'Market Limit updated.', value: cl.market_limit }
-            else
-              render json: { success: false, message: cl.errors.full_messages }
-            end
-          end
-        else
-          render json: { errors: "Not authenticated", response_code: 201 }
+          render json: {errors: "Not authenticated", response_code: 201}
         end
       end
 
@@ -73,18 +28,18 @@ module Api
         if current_company
           buyer = Company.where(id: params[:buyer_id]).first
           if buyer.nil?
-            render json: { success: false, message: "Buyer doesn't exist" }
+            render json: {success: false, message: "Buyer doesn't exist"}
           else
             dl = DaysLimit.where(buyer_id: params[:buyer_id], seller_id: current_company.id).first_or_initialize
             dl.days_limit = params[:limit]
             if dl.save
-              render json: { success: true, message: 'Days Limit updated.', value: view_context.get_days_limit(buyer, current_company) }
+              render json: {success: true, message: 'Days Limit updated.', value: view_context.get_days_limit(buyer, current_company)}
             else
-              render json: { success: false, message: dl.errors.full_messages }
+              render json: {success: false, message: dl.errors.full_messages}
             end
           end
         else
-          render json: { errors: "Not authenticated", response_code: 201 }
+          render json: {errors: "Not authenticated", response_code: 201}
         end
       end
 
@@ -93,15 +48,15 @@ module Api
           unless params[:company_id].nil? || params[:company_id].blank?
             bu = BlockUser.where(block_company_ids: params[:company_id], company_id: current_company.id).first_or_initialize
             if bu.save
-              render json: { success: true }
+              render json: {success: true}
             else
-              render json: { errors: bu.errors.full_messages, response_code: 201 }
+              render json: {errors: bu.errors.full_messages, response_code: 201}
             end
           else
-            render json: { errors: "Parameters missing", response_code: 201 }
+            render json: {errors: "Parameters missing", response_code: 201}
           end
         else
-          render json: { errors: "Not authenticated", response_code: 201 }
+          render json: {errors: "Not authenticated", response_code: 201}
         end
       end
 
@@ -109,13 +64,13 @@ module Api
         if current_company
           b = BlockUser.where(block_company_ids: params[:company_id], company_id: current_company.id).first
           if b.nil?
-            render json: { success: false, message: 'Company already unblocked or not found' }
+            render json: {success: false, message: 'Company already unblocked or not found'}
           else
             b.destroy
-            render json: { success: true }
+            render json: {success: true}
           end
         else
-          render json: { errors: "Not authenticated", response_code: 201 }
+          render json: {errors: "Not authenticated", response_code: 201}
         end
       end
 
@@ -124,19 +79,19 @@ module Api
           if params[:company_id].present?
             company = Company.where(id: params[:company_id]).first
             if company.nil?
-             render json: { success: false, errors: "Company not found", response_code: 201 }
+              render json: {success: false, errors: "Company not found", response_code: 201}
             else
               @data = {
-                id: company.id.to_s,
-                name: company.try(:name),
-                total_limit: get_credit_limit(company, current_company),
-                used_limit: get_used_credit_limit(company, current_company),
-                available_limit: get_available_credit_limit(company, current_company),
-                overdue_limit: get_days_limit(company, current_company),
-                market_limit: get_market_limit_from_credit_limit_table(company, current_company).to_s,
-                supplier_connected: supplier_connected(company, current_company).to_s
+                  id: company.id.to_s,
+                  name: company.try(:name),
+                  total_limit: get_credit_limit(company, current_company),
+                  used_limit: get_used_credit_limit(company, current_company),
+                  available_limit: get_available_credit_limit(company, current_company),
+                  overdue_limit: get_days_limit(company, current_company),
+                  market_limit: get_market_limit_from_credit_limit_table(company, current_company).to_s,
+                  supplier_connected: supplier_connected(company, current_company).to_s
               }
-              render json: { success: true, limits: @data, response_code: 200  }
+              render json: {success: true, limits: @data, response_code: 200}
             end
           else
             @data = []
@@ -144,7 +99,7 @@ module Api
             @array = []
             credit_limit.each do |c|
               if c.buyer.present?
-                if c.credit_limit > 0 || (!c.market_limit.nil? && c.market_limit > 0) 
+                if c.credit_limit > 0 || (!c.market_limit.nil? && c.market_limit > 0)
                   @array << c.buyer_id
                 end
               end
@@ -152,40 +107,40 @@ module Api
             days_limit = DaysLimit.where(seller_id: current_company.id)
             days_limit.each do |dl|
               if dl.buyer.present?
-                if  (dl.days_limit != 30) && dl.days_limit > 0
+                if (dl.days_limit != 30) && dl.days_limit > 0
                   @array << dl.buyer_id
                 end
               end
             end
             companies = Company.where("id in (?)", @array.uniq)
             companies.each do |c|
-                @data << {
-                      id: c.id.to_s,
-                      name: c.try(:name),
-                      total_limit: get_credit_limit(c, current_company),
-                      used_limit: get_used_credit_limit(c, current_company),
-                      available_limit: get_available_credit_limit(c, current_company),
-                      overdue_limit: get_days_limit(c, current_company),
-                      market_limit: get_market_limit_from_credit_limit_table(c,current_company).to_s,
-                      supplier_connected: supplier_connected(c,current_company).to_s }
+              @data << {
+                  id: c.id.to_s,
+                  name: c.try(:name),
+                  total_limit: get_credit_limit(c, current_company),
+                  used_limit: get_used_credit_limit(c, current_company),
+                  available_limit: get_available_credit_limit(c, current_company),
+                  overdue_limit: get_days_limit(c, current_company),
+                  market_limit: get_market_limit_from_credit_limit_table(c, current_company).to_s,
+                  supplier_connected: supplier_connected(c, current_company).to_s}
             end
             @companies = Kaminari.paginate_array(@data).page(params[:page]).per(params[:count])
-            render json: { success: true, pagination: set_pagination(:companies), limits: @companies, response_code: 200  }
+            render json: {success: true, pagination: set_pagination(:companies), limits: @companies, response_code: 200}
           end
         else
-          render json: { success: false, errors: "You have to login first!!", response_code: 201 }
+          render json: {success: false, errors: "You have to login first!!", response_code: 201}
         end
       end
 
       def add_star
-       stone = Stone.find_by(id: params[:stone_id])
-       rating = Rating.new()
-       rating.key = stone.description+'#'+(stone.weight.to_f).to_s
-       rating.flag_type = 'Imp'
-       rating.tender_id = params[:tender_id]
-       rating.customer_id = current_company.id
-       rating.save!
-       render json: { success: true, response_code: 200  }
+        stone = Stone.find_by(id: params[:stone_id])
+        rating = Rating.new()
+        rating.key = stone.description + '#' + (stone.weight.to_f).to_s
+        rating.flag_type = 'Imp'
+        rating.tender_id = params[:tender_id]
+        rating.customer_id = current_company.id
+        rating.save!
+        render json: {success: true, response_code: 200}
       end
     end
   end
