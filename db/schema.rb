e204290727d9a3ebe493ac79dc025645ea0812e3
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-11-28 18:03:07 +0500

    database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181124161413) do
+ActiveRecord::Schema.define(version: 20181128112822) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -147,8 +147,9 @@ ActiveRecord::Schema.define(version: 20181124161413) do
     t.text "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "group_market_limit"
+    t.decimal "group_market_limit", precision: 10
     t.integer "group_overdue_limit"
+    t.decimal "credit_limit", precision: 10
   end
 
   create_table "contact_people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -648,6 +649,7 @@ ActiveRecord::Schema.define(version: 20181124161413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "supplier_unpaid", default: 0
+    t.decimal "percentage", precision: 10, default: "0"
   end
 
   create_table "seller_scores", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
