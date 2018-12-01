563975f7ec4d98581d4996d56670cc574d2402d4
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-12-01 13:33:22 +0500

    supplier connected status with pending,overdue and completed

--- app/models/company.rb
+++ app/models/company.rb
@@ -551,7 +551,7 @@ class Company < ApplicationRecord
 
   def supplier_connected
     #dummy_co = Company.where(name: "Dummy co. 1").first
-    self.buyer_transactions.select(:seller_id).where("paid = ?", true).uniq.count
+    self.buyer_transactions.select(:seller_id).uniq.count
   end
 
   def supplier_paid
