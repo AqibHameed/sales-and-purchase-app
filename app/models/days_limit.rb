945940fc7341a7ddcd1a81fa4974f7a4f8cf0f84
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-12-10 14:33:54 +0500

    revert background job

--- app/models/days_limit.rb
+++ app/models/days_limit.rb
@@ -1,7 +1,8 @@
 class DaysLimit < ApplicationRecord
   include LiveMonitor
   validates_numericality_of :days_limit
-  after_save :update_secure_center
+  #after_save :update_secure_center
+  after_save :secure_center
   belongs_to :buyer, class_name: 'Company', foreign_key: 'buyer_id'
   belongs_to :seller, class_name: 'Company', foreign_key: 'seller_id'
 end
