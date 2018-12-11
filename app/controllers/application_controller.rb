47ce045eb9bf3b4baa8578140a169f9f893e0ad8
Khuram R <khuramdogar73@gmail.com>
2018-12-11 15:59:03 +0500

    error resolved csrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper :all
-  before_action :set_paper_trail_whodunnit
   protect_from_forgery with: :exception
+  before_action :set_paper_trail_whodunnit
   helper_method :current_company
   
   before_action :configure_permitted_parameters, if: :devise_controller?
