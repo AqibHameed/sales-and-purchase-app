31a83037800c6d79cf042062d4d44cd9d2ceff4e
Khuram R <khuramdogar73@gmail.com>
2018-12-14 13:19:49 +0500

    single transaction and paid transaction tested successfully

--- spec/concerns/live_monitor_spec.rb
+++ spec/concerns/live_monitor_spec.rb
@@ -14,16 +14,41 @@ describe LiveMonitor do
                                 role: "Buyer/Seller", confirmed_at: Time.current, company: buyer_company, authentication_token: Devise.friendly_token)
     create(:customer_role, customer: @customer)
     @parcel = create(:trading_parcel, customer: @customer, company: @customer.company)
+    @parcel1 = create(:trading_parcel, customer: @customer, company: @customer.company)
   end
-  before(:each) do
-    @transaction = Transaction.create!(buyer_id: @buyer.company_id, seller_id: @customer.company_id, trading_parcel_id: 1, due_date: 15.days.ago, created_at: 10.days.ago)
-  end
+  # before(:each) do
+  #
+  # end
   describe '#update_secure_center' do
     context 'when call secure center' do
-      it 'does show secure center' do
-        @transaction.secure_center
+      it 'does show paid date equal to nil' do
+        transaction = Transaction.create!(buyer_id: @buyer.company_id,
+                                           seller_id: @customer.company_id,
+                                           trading_parcel_id: @parcel1.id,
+                                           due_date: 15.days.ago,
+                                           created_at: 10.days.ago,
+                                           price: 100,
+                                           buyer_confirmed: true,
+                                           remaining_amount: 200)
+        transactions = transaction.secure_center
+        transactions.last.paid_date.should be nil
       end
     end
-
+    context 'when transaction paid' do
+      it 'does show the last paid date' do
+        transaction = Transaction.create!(buyer_id: @buyer.company_id,
+                                          seller_id: @customer.company_id,
+                                          trading_parcel_id: @parcel1.id,
+                                          due_date: 15.days.ago,
+                                          created_at: 10.days.ago,
+                                          price: 100,
+                                          buyer_confirmed: true,
+                                          remaining_amount: 0,
+                                          paid: false)
+        partial = PartialPayment.create(company_id: @buyer.company_id, transaction_id: transaction.id, amount: 50,)
+        transactions = transaction.secure_center
+        expect(Date.current).to eq(transactions.last.paid_date)
+      end
+    end
   end
 end
