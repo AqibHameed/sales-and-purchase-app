b9a498ba43e8fdfa150406e784bc0933c1418185
Khuram R <khuramdogar73@gmail.com>
2018-12-14 16:56:48 +0500

    4 test cases written

--- spec/concerns/live_monitor_spec.rb
+++ spec/concerns/live_monitor_spec.rb
@@ -16,38 +16,39 @@ describe LiveMonitor do
     @parcel = create(:trading_parcel, customer: @customer, company: @customer.company)
     @parcel1 = create(:trading_parcel, customer: @customer, company: @customer.company)
   end
-  # before(:each) do
-  #
-  # end
+  before(:each) do
+    @transaction = Transaction.create!(buyer_id: @buyer.company_id,
+                                      seller_id: @customer.company_id,
+                                      trading_parcel_id: @parcel1.id,
+                                      due_date: Date.current + 30,
+                                      created_at: 10.days.ago,
+                                      buyer_confirmed: true,
+                                      remaining_amount: 100,
+                                      paid: false)
+  end
   describe '#update_secure_center' do
     context 'when call secure center' do
       it 'does show paid date equal to nil' do
-        transaction = Transaction.create!(buyer_id: @buyer.company_id,
-                                           seller_id: @customer.company_id,
-                                           trading_parcel_id: @parcel1.id,
-                                           due_date: 15.days.ago,
-                                           created_at: 10.days.ago,
-                                           price: 100,
-                                           buyer_confirmed: true,
-                                           remaining_amount: 200)
-        transactions = transaction.secure_center
+        transactions = @transaction.secure_center
         transactions.last.paid_date.should be nil
       end
     end
     context 'when transaction paid' do
       it 'does show the last paid date' do
-        transaction = Transaction.create!(buyer_id: @buyer.company_id,
-                                          seller_id: @customer.company_id,
-                                          trading_parcel_id: @parcel1.id,
-                                          due_date: 15.days.ago,
-                                          created_at: 10.days.ago,
-                                          price: 100,
-                                          buyer_confirmed: true,
-                                          remaining_amount: 0,
-                                          paid: false)
-        partial = PartialPayment.create(company_id: @buyer.company_id, transaction_id: transaction.id, amount: 50,)
-        transactions = transaction.secure_center
+        partial = PartialPayment.create(company_id: @buyer.company_id, transaction_id: @transaction.id, amount: 50,)
+        transactions = @transaction.secure_center
         expect(Date.current).to eq(transactions.last.paid_date)
+      end
+      it 'does show supplier paid increased' do
+        partial = PartialPayment.create(company_id: @buyer.company_id, transaction_id: @transaction.id, amount: 50,)
+        transactions = @transaction.secure_center
+        expect(1).to eq(transactions.last.supplier_paid)
+      end
+    end
+    context 'when over due that passed away' do
+      it 'does show over due amount' do
+        @transaction.update_attributes!(due_date: 30.days.ago)
+        transactions = @transaction.secure_center
       end
     end
   end
