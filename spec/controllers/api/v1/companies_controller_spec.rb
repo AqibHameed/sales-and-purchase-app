a395bb561281288f0f99e965c53d5cc1a5e4209c
Khuram R <khuramdogar73@gmail.com>
2018-12-15 20:43:12 +0500

    fix error parcel must exist

--- spec/controllers/api/v1/companies_controller_spec.rb
+++ spec/controllers/api/v1/companies_controller_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Api::V1::CompaniesController do
   before(:each) do
     request.headers.merge!(authorization: @customer.authentication_token)
     1.upto(5) do
+      @parcel = create(:trading_parcel, customer: @customer, company: @customer.company)
       create(:transaction, buyer_id: @buyer.company_id,
              seller_id: @customer.company_id,
              trading_parcel_id: @parcel.id)
@@ -38,18 +39,6 @@ RSpec.describe Api::V1::CompaniesController do
                                              buyer_id: @buyer.company_id)
         expect(response.status).to eq(200)
       end
-      # context 'when  create transaction against single seller' do
-      #   it 'does increase outstanding amount if paid is false ' do
-      #     get 'live_monitoring', params: {id: @buyer.company_id}
-      #     total_amount = TradingParcel.where(customer: @customer).sum(:total_value)
-      #     assigns(:secure_center).outstandings.should eq(total_amount)
-      #     create(:transaction, buyer_id: @buyer.company_id,
-      #            seller_id: @customer.company_id,
-      #            trading_parcel_id: @parcel.id)
-      #     get 'live_monitoring', params: {id: @buyer.company_id}
-      #     assigns(:secure_center).outstandings.should eq(total_amount)
-      #   end
-      # end
     end
   end
 end
