1bfbed9bccc3768456e3c0281dad842370aa79ac
Khuram R <khuramdogar73@gmail.com>
2018-12-31 22:01:32 +0500

    write apis for broker and buyer seller

--- app/controllers/api/v1/brokers_controller.rb
+++ app/controllers/api/v1/brokers_controller.rb
@@ -1,8 +1,8 @@
 module Api
   module V1
     class BrokersController < ApiController
-      before_action :load_request , only: [:reject, :remove]
-      skip_before_action :verify_authenticity_token, only: [:send_request, :company_record_on_the_basis_of_roles]
+      before_action :check_current_company_requests , only: [:accept, :reject, :remove]
+      skip_before_action :verify_authenticity_token, only: [:send_request, :company_record_on_the_basis_of_roles, :accept, :reject, :remove]
 
       include ApplicationHelper
 
@@ -135,24 +135,55 @@ module Api
         end
       end
 
+=begin
+ @apiVersion 1.0.0
+ @api {post} /api/v1/brokers/send_request
+ @apiSampleRequest off
+ @apiName Send request
+ @apiGroup Brokers
+ @apiDescription send request seller to broker, broker to seller
+ @apiParamExample {json} Request-Example:
+{
+	"company": "broker1(Broker)"
+}
+ @apiSuccessExample {json} SuccessResponse:
+{
+    "success": true,
+    "message": "Request sent successfully"
+}
+=end
+
       def send_request
         if current_company
-          seller = Company.where("name LIKE ?", params[:company]).first
-          if seller.present?
-            broker_request = BrokerRequest.where(broker_id: current_company.id, seller_id: seller.id).first_or_initialize do |br|
-              br.accepted = false
-            end
-            if broker_request.save
-              Message.create_new_broker(broker_request, current_company)
-              render json: { success: true, message: "Request sent successfully" }
+          company = Company.where("name LIKE ?", params[:company]).first
+          if company.present?
+            if current_company.is_broker?
+              request = BrokerRequest.where(broker_id: current_company.id,
+                                                   seller_id: company.id,
+                                                   sender_id: current_company.id,
+                                                   receiver_id: company.id).first_or_initialize do |br|
+                br.accepted = false
+              end
             else
-              render json: { success: false, message: "Unable to send request." }
+              request = BrokerRequest.where(broker_id: company.id,
+                                                   seller_id: current_company.id,
+                                                   sender_id: current_company.id,
+                                                   receiver_id: company.id).first_or_initialize do |br|
+                br.accepted = false
+              end
+            end
+            if request.save
+              Message.create_new_broker(request, current_company) if current_company.is_broker?
+              Message.create_new_seller(request, current_company) unless current_company.is_broker?
+              render json: {success: true, message: "Request sent successfully"}
+            else
+              render json: {success: false, message: "Unable to send request."}
             end
           else
-            render json: { success: false, message: "There is no company with this name" }
+            render json: {success: false, message: "There is no company with this name"}
           end
         else
-          render json: { errors: "Not authenticated", response_code: 201 }
+          render json: {errors: "Not authenticated", response_code: 201}
         end
       end
 
@@ -181,12 +212,222 @@ module Api
           render json: { errors: "Not authenticated", response_code: 201 }
         end
       end
-    end
 
-    private
+=begin
+ @apiVersion 1.0.0
+ @api {post} /api/v1/brokers/accept
+ @apiSampleRequest off
+ @apiName Accept request
+ @apiGroup Brokers
+ @apiDescription accept request seller to broker or broker to seller
+ @apiParamExample {json} Request-Example:
+{
+	"request_id": 3
+}
+ @apiSuccessExample {json} SuccessResponse:
+{
+  success: true,
+  message: 'Request accepted successfully.',
+  status: 200
+}
+=end
 
-    def load_request
-      @broker_request = BrokerRequest.find(params[:id])
+      def accept
+        render json: { success: true,
+                       message: 'Request accepted successfully.',
+                       response_code: 200 } if @broker_request.update_column(:accepted, true)
+      end
+
+=begin
+ @apiVersion 1.0.0
+ @api {post} /api/v1/brokers/reject
+ @apiSampleRequest off
+ @apiName reject request
+ @apiGroup Brokers
+ @apiDescription reject request seller to broker or broker to seller
+ @apiParamExample {json} Request-Example:
+{
+	"request_id": 3
+}
+ @apiSuccessExample {json} SuccessResponse:
+{
+  success: true,
+  message: 'Request rejected successfully.',
+  status: 200
+}
+=end
+
+      def reject
+        render json: { success: true,
+                       message: 'Request rejected successfully.',
+                       response_code: 200 } if @broker_request.destroy
+      end
+
+=begin
+ @apiVersion 1.0.0
+ @api {post} /api/v1/brokers/remove
+ @apiSampleRequest off
+ @apiName remove request
+ @apiGroup Brokers
+ @apiDescription reomve  seller or broker
+ @apiParamExample {json} Request-Example:
+{
+	"request_id": 3
+}
+ @apiSuccessExample {json} SuccessResponse:
+{
+  success: true,
+  message: 'You have removed successfully.',
+  status: 200
+}
+=end
+
+      def remove
+        if @broker_request.destroy
+          CustomerMailer.remove_broker_mail(@broker_request).deliver
+          render json: { success: true,
+                         message: 'You have removed successfully.',
+                         response_code: 200 }
+        end
+      end
+
+=begin
+ @apiVersion 1.0.0
+ @api {get} /api/v1/brokers/show_requests
+ @apiSampleRequest off
+ @apiName show requests
+ @apiGroup Brokers
+ @apiDescription show requests incoming from sellers/buyers or broker
+ @apiSuccessExample {json} SuccessResponse:
+{
+    "success": true,
+    "pagination": {
+        "total_pages": 1,
+        "prev_page": null,
+        "next_page": null,
+        "current_page": 1
+    },
+    "response_code": 200,
+    "requests": [
+        {
+            "request_id": 8,
+            "broker_name": "broker 1",
+            "seller_buyer_name": "broker1(Broker)"
+        }
+    ]
+}
+=end
+
+      def show_requests
+        if current_company
+          @requests_data = []
+          if current_company.is_broker?
+            @requests = BrokerRequest.where(broker_id: current_company.id,
+                                            accepted: false,
+                                            receiver_id: current_company.id)
+            @requests.each do |request|
+              @requests_data << {
+                  request_id: request.id,
+                  seller_buyer_name: request.seller.customers.first.name,
+                  broker_name: request.seller.name
+              }
+            end unless @requests.nil?
+          else
+            @requests = BrokerRequest.where(seller_id: current_company.id,
+                                            accepted: false,
+                                            receiver_id: current_company.id)
+            @requests.each do |request|
+              @requests_data << {
+                  request_id: request.id,
+                  broker_name: request.broker.customers.first.name,
+                  seller_buyer_name: request.broker.name
+              }
+            end unless @requests.nil?
+          end
+          @all_requests = Kaminari.paginate_array(@requests_data).page(params[:page]).per(params[:count])
+          render json: { success: true,
+                         pagination: set_pagination(:all_requests),
+                         response_code: 200,
+                         requests: @requests_data}
+        else
+          render json: { errors: "Not authenticated", response_code: 201 }
+        end
+      end
+
+=begin
+ @apiVersion 1.0.0
+ @api {get} /api/v1/brokers/show_myclients
+ @apiSampleRequest off
+ @apiName show all connected sellers/buyers or brokers
+ @apiGroup Brokers
+ @apiDescription show connected sellers/buyers or broker
+ @apiSuccessExample {json} SuccessResponse:
+{
+    "success": true,
+    "pagination": {
+        "total_pages": 1,
+        "prev_page": null,
+        "next_page": null,
+        "current_page": 1
+    },
+    "response_code": 200,
+    "myclients": [
+        {
+            "request_id": 9,
+            "broker_name": "broker 1",
+            "seller_buyer_name": "broker1(Broker)"
+        }
+    ]
+}
+=end
+
+      def show_myclients
+        if current_company
+          @my_clients_data = []
+          if current_company.is_broker?
+            @my_sellers = BrokerRequest.where(broker_id: current_company.id,
+                                              accepted: true)
+            @my_sellers.each do |mysellers|
+              @my_clients_data << {
+                  request_id: mysellers.id,
+                  seller_buyer_name: mysellers.brokers.customers.first.name,
+                  broker_name: mysellers.brokers.name
+              }
+            end unless @my_sellers.nil?
+          else
+            @my_brokers = BrokerRequest.where(seller_id: current_company.id,
+                                              accepted: true)
+            @my_brokers.each do |mybroker|
+              @my_clients_data << {
+                  request_id: mybroker.id,
+                  broker_name: mybroker.broker.customers.first.name,
+                  seller_buyer_name: mybroker.broker.name
+              }
+            end unless @my_brokers.nil?
+          end
+          @all_clients = Kaminari.paginate_array(@my_clients_data).page(params[:page]).per(params[:count])
+          render json: { success: true,
+                         pagination: set_pagination(:all_clients),
+                         response_code: 200,
+                         myclients: @my_clients_data }
+        else
+          render json: { errors: "Not authenticated", response_code: 201 }
+        end
+      end
+
+      private
+
+      def check_current_company_requests
+        if current_company
+          @broker_request = BrokerRequest.find_by(id: params[:request_id], receiver_id: current_company.id)
+          if @broker_request.nil?
+            render json: { success: true, message: "#{current_company.name} don't have any request with this id #{params[:request_id]}" }
+          end
+        else
+          render json: { errors: "Not authenticated", response_code: 201 }
+        end
+      end
+
     end
   end
 end
--- public/api/api_project.js
+++ public/api/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-29T17:20:35.268Z",
+    "time": "2018-12-31T16:50:24.318Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
