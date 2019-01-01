c8cdfcd318cd7b4ecd192540cb958e56cf2e94e3
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-01-01 13:29:53 +0500

    remove error from migration

--- db/migrate/20181231081241_change_column_to_broker_request.rb
+++ db/migrate/20181231081241_change_column_to_broker_request.rb
@@ -1,7 +1,5 @@
 class ChangeColumnToBrokerRequest < ActiveRecord::Migration[5.1]
   def change
-    remove_column :broker_requests, :send
-    remove_column :broker_requests, :receive
     add_column :broker_requests, :sender_id, :integer
     add_column :broker_requests, :receiver_id, :integer
   end
