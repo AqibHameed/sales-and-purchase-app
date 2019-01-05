f1fe8e614f344def5f33593bb04287b943fbe83a
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-01-05 18:59:13 +0500

    message show test cases

--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -3,6 +3,13 @@ RSpec.describe Api::V1::MessagesController do
   before(:all) do
     create_roles
     @customer = create_customer
+    @buyer = create_buyer
+    @parcel = create(:trading_parcel, customer: @buyer, company: @customer.company)
+    @proposal = create(:proposal, buyer_id: @buyer.company.id,
+           seller_id: @customer.company.id,
+           trading_parcel_id: @parcel.id,
+           price: "4000", credit: "1500")
+    @message = create(:message, proposal_id: @proposal.id)
   end
   before(:each) do
     request.headers.merge!(authorization: @customer.authentication_token)
@@ -26,6 +33,37 @@ RSpec.describe Api::V1::MessagesController do
         response.body.should have_content('Message Created Successfully')
       end
     end
+  end
+
+  describe '#show' do
+    context 'when authorized user to show the message' do
+      it 'does show error un authorized user' do
+        request.headers.merge!(authorization: 'wetasdetoken')
+        put :show, params: {id: 'al'}
+        response.body.should have_content('Not authenticated')
+      end
+    end
+
+    context 'when authorized user to show the message if message id not exist' do
+      it 'does show the message Message id should be present' do
+        put :show, params: {id: ''}
+        response.body.should have_content('Message id should be present')
+      end
+    end
+
+    context 'when authorized user to show the message if message is not exist' do
+      it 'does show the message Message id should be present' do
+        put :show, params: {id: 'al'}
+        response.body.should have_content('Message does not exists for this id')
+      end
+    end
+
+    # context 'when authorized user to show the message if message is  exist' do
+    #   it 'does show the message Message id should be present' do
+    #     put :show, params: {id: @message.id}
+    #     response.body.should have_content('')
+    #   end
+    # end
 
   end
 end
