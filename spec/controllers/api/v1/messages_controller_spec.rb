43717ec959466c65d60bf677c6bee0c0af100612
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-01-08 14:46:46 +0500

    message controller test cases

--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -10,10 +10,79 @@ RSpec.describe Api::V1::MessagesController do
            trading_parcel_id: @parcel.id,
            price: "4000", credit: "1500")
     @message = create(:message, proposal_id: @proposal.id, receiver_id: @customer.company.id, sender_id: @buyer.company.id)
+    @proposal_message = Message.create_new(@proposal)
+    @negotiation = create(:negotiation, proposal: @proposal)
   end
   before(:each) do
     request.headers.merge!(authorization: @customer.authentication_token)
   end
+
+  describe '#index' do
+    context 'when unauthorized user to access the message' do
+      it 'does show error un authorized user' do
+        request.headers.merge!(authorization: 'wetasdetoken')
+        get :index
+        response.body.should have_content('Not authenticated')
+      end
+    end
+
+    context 'when authorized user to access the message' do
+      it 'does show the message you have new proposal' do
+        get :index
+        expect(@proposal_message.subject).to eq(JSON.parse(response.body)['messages'].first['subject'])
+      end
+    end
+
+    context 'when authorized user to access the message' do
+      it 'does show the message Your proposal is accepted' do
+        proposal = create(:proposal, buyer_id: @buyer.company.id,
+                          seller_id: @customer.company.id,
+                          trading_parcel_id: @parcel.id,
+                          price: "4000", credit: "1500", status: 1)
+        accept_proposal = Message.accept_proposal(proposal, @customer)
+        get :index, params: {status: 'accepted'}
+        expect(accept_proposal.subject).to eq('Your proposal is accepted.')
+      end
+    end
+
+    context 'when authorized user to access the message' do
+      it 'does show the message Your proposal is rejected' do
+        proposal = create(:proposal, buyer_id: @buyer.company.id,
+                          seller_id: @customer.company.id,
+                          trading_parcel_id: @parcel.id,
+                          price: "4000", credit: "1500", status: 2)
+        reject_proposal = Message.reject_proposal(proposal, @customer)
+        get :index
+        expect(reject_proposal.subject).to eq('Your proposal is rejected.')
+      end
+    end
+
+    context 'when authorized user to access the message' do
+      it 'does show the message Buyer accepted your negotiated proposal' do
+        proposal = create(:proposal, buyer_id: @buyer.company.id,
+                          seller_id: @customer.company.id,
+                          trading_parcel_id: @parcel.id,
+                          price: "4000", credit: "1500", status: 2)
+        buyer_accept_proposal = Message.buyer_accept_proposal(proposal, @customer)
+        get :index
+        expect(buyer_accept_proposal.subject).to eq('Buyer accepted your negotiated proposal.')
+      end
+    end
+
+    context 'when authorized user to access the message' do
+      it 'does show the message Buyer rejected your negotiated proposal' do
+        proposal = create(:proposal, buyer_id: @buyer.company.id,
+                          seller_id: @customer.company.id,
+                          trading_parcel_id: @parcel.id,
+                          price: "4000", credit: "1500", status: 2)
+        buyer_reject_proposal = Message.buyer_reject_proposal(proposal, @customer)
+        get :index
+        expect(buyer_reject_proposal.subject).to eq('Buyer rejected your negotiated proposal.')
+        expect(response.success?).to eq(true)
+      end
+    end
+  end
+
   describe '#create' do
     context 'when unauthorized user to create the message' do
       it 'does show error un authorized user' do
@@ -37,7 +106,7 @@ RSpec.describe Api::V1::MessagesController do
   end
 
   describe '#show' do
-    context 'when authorized user to show the message' do
+    context 'when unauthorized user to show the message' do
       it 'does show error un authorized user' do
         request.headers.merge!(authorization: 'wetasdetoken')
         put :show, params: {id: 'al'}
@@ -68,5 +137,23 @@ RSpec.describe Api::V1::MessagesController do
       end
     end
 
+  end
+
+  describe '#unread_count' do
+    context 'when unauthorized user to read the unread message count' do
+      it 'does show error un authorized user' do
+        request.headers.merge!(authorization: 'wetasdetoken')
+        get :unread_count
+        response.body.should have_content('Not authenticated')
+      end
+    end
+
+    context 'when authorized user to read the unread message count' do
+      it 'does show number of count message' do
+        get :unread_count
+        JSON.parse(response.body)['inbox']['count'].should be > 0
+        expect(response.success?).to eq(true)
+      end
+    end
   end
 end
