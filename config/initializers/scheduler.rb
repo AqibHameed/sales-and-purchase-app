572047b4f8ba6d9ac74fda9ea9aac71c7b66ddbd
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-01-15 21:42:27 +0500

    cron job

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,30 +2,30 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '5 0 * * *' do
+scheduler.cron '0 5 * * *' do
   Transaction.send_overdue_email
 end
 
-scheduler.cron '* * * * *' do
+scheduler.cron '0 8 * * *' do
   Tender.update_round_prices
 end
 
-scheduler.every '7h10m' do
+scheduler.cron '0 12 * * *' do
   Demand.update_demands_block_unblock
 end
 
-scheduler.every '7h15m' do
+scheduler.cron '0 15 * * *' do
   PolishedDemand.update_polished_demands_block_unblock
 end
 
-scheduler.cron '0 5 * * *' do
+scheduler.cron '0 18 * * *' do
   ApplicationHelper.update_scores
 end
 
-scheduler.cron '3 1 * * *' do
+scheduler.cron '3 20 * * *' do
   Transaction.direct_sell_confirm
 end
 
-scheduler.cron '0 1 * * *' do
+scheduler.cron '0 23 * * *' do
   Transaction.sell_confirm_auto
 end
