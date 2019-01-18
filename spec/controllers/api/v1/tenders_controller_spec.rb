55099d1712931f15c6102bae025e929ec6ff130a
UsmanAzmat300 <d.usmanazmat@gmil.com>
2019-01-18 21:17:36 +0500

    adding_test_cases

---
+++ spec/controllers/api/v1/tenders_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+RSpec.describe Api::V1::TendersController do
+  before(:all) do
+    # create_roles
+    @supplier = create(:supplier)
+    # @tender = create_tender
+    @tender = create(:tender, supplier: @supplier, open_date: DateTime.now - 1, close_date: DateTime.now + 2)
+  end
+
+  describe '#tender_index' do
+
+    context 'when authorized  tender access the api' do
+      it 'get the tender according to loation' do
+        get :index, params: {location: @tender.country}
+        expect(JSON.parse(response.body)['tenders'].first['country']).to eq(@tender.country)
+      end
+    end
+
+    context 'when authorized  tender access the api' do
+      it 'get the tender according to month' do
+        get :index, params: {month: @tender.open_date.to_date.mon}
+      expect(JSON.parse(response.body)['tenders'].first['start_date'].to_date.mon).to eq(@tender.open_date.to_date.mon)
+      end
+    end
+    context 'when authorized  tender access the api' do
+      it 'get the tender according to supplier_id' do
+        get :index, params: {supplier: @tender.supplier_id}
+        tender=Tender.find_by(id: JSON.parse(response.body)['tenders'].first['id'])
+        expect(tender.supplier.name).to eq(@tender.supplier.name)
+      end
+    end
+
+
+
+
+    context 'when authorized  tender access the api' do
+      it 'get the tender according to upcoming' do
+        tender = create(:tender, supplier: @supplier, open_date:  DateTime.now + 1, close_date:  DateTime.now + 2)
+        get :index
+        expect(JSON.parse(response.body)['tenders'].last['start_date'].to_datetime).to be  > DateTime.now
+      end
+    end
+
+    context 'when authorized  tender access the api' do
+      it 'get the tender according to past' do
+        tender = create(:tender, supplier: @supplier, open_date:  DateTime.now - 2, close_date:  DateTime.now - 1)
+        get :index
+        expect(JSON.parse(response.body)['tenders'].first['end_date'].to_datetime).to be  < DateTime.now
+      end
+    end
+
+  end
+end
+
+
+
+
