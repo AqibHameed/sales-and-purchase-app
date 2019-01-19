7fca512b416c9ff9be2c5946d5aa495ec7b1524a
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-01-19 14:19:51 +0500

    tenders spec messages change

--- spec/controllers/api/v1/tenders_controller_spec.rb
+++ spec/controllers/api/v1/tenders_controller_spec.rb
@@ -11,27 +11,27 @@ RSpec.describe Api::V1::TendersController do
   describe '#tender_index' do
 
     context 'when authorized  tender access the api' do
-      it 'get the tender according to loation' do
+      it 'does show the tender according to location' do
         get :index, params: {location: @tender.country}
         expect(JSON.parse(response.body)['tenders'].first['country']).to eq(@tender.country)
       end
     end
 
     context 'when authorized  tender access the api' do
-      it 'get the tender according to month' do
+      it 'does shows the tender according to month' do
         get :index, params: {month: @tender.open_date.to_date.mon}
         expect(JSON.parse(response.body)['tenders'].first['start_date'].to_date.mon).to eq(@tender.open_date.to_date.mon)
       end
     end
     context 'when authorized  tender access the api' do
-      it 'get the tender according to supplier_id' do
+      it 'does show the tender according to supplier_id' do
         get :index, params: {supplier: @tender.supplier_id}
         tender = Tender.find_by(id: JSON.parse(response.body)['tenders'].first['id'])
         expect(tender.supplier.name).to eq(@tender.supplier.name)
       end
     end
     context 'when authorized  tender access the api' do
-      it 'get the tender according to upcoming' do
+      it 'does show the upcoming tenders' do
         tender = create(:tender, supplier: @supplier, open_date: DateTime.now + 1, close_date: DateTime.now + 2)
         get :index
         expect(JSON.parse(response.body)['tenders'].last['start_date'].to_datetime).to be > DateTime.now
@@ -39,7 +39,7 @@ RSpec.describe Api::V1::TendersController do
     end
 
     context 'when authorized  tender access the api' do
-      it 'get the tender according to past' do
+      it 'does show the past tenders' do
         tender = create(:tender, supplier: @supplier, open_date: DateTime.now - 2, close_date: DateTime.now - 1)
         get :index
         expect(JSON.parse(response.body)['tenders'].first['end_date'].to_datetime).to be < DateTime.now
@@ -55,27 +55,27 @@ RSpec.describe Api::V1::TendersController do
 
   describe '#tender_upcomming' do
     context 'when authorized  tender access the api' do
-      it 'get the tender according to upcoming' do
+      it 'does show the upcoming tenders' do
         get :index
         expect(JSON.parse(response.body)['tenders'].last['start_date'].to_datetime).to be > DateTime.now
       end
     end
 
     context 'when authorized  tender access the api' do
-      it 'get the tender according to loation' do
+      it 'does show the tender according to loation' do
         get :index, params: {location: @tender.country}
         expect(JSON.parse(response.body)['tenders'].first['country']).to eq(@tender.country)
       end
     end
 
     context 'when authorized  tender access the api' do
-      it 'get the tender according to month' do
+      it 'does shiw the tender according to month' do
         get :index, params: {month: @tender.open_date.to_date.mon}
         expect(JSON.parse(response.body)['tenders'].first['start_date'].to_date.mon).to eq(@tender.open_date.to_date.mon)
       end
     end
     context 'when authorized  tender access the api' do
-      it 'get the tender according to supplier_id' do
+      it 'does show the tender according to supplier_id' do
         get :index, params: {supplier: @tender.supplier_id}
         tender = Tender.find_by(id: JSON.parse(response.body)['tenders'].first['id'])
         expect(tender.supplier.name).to eq(@tender.supplier.name)
