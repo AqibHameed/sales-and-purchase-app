26a7d98914e5f6de452811a7d1a9c966bb3d12fc
Khuram R <khuramdogar73@gmail.com>
2019-01-19 21:42:54 +0500

    resolve error

--- app/models/premission_request.rb
+++ app/models/premission_request.rb
@@ -1,6 +1,6 @@
 class PremissionRequest < ApplicationRecord
   belongs_to :sender, class_name: 'Company', foreign_key: 'sender_id', optional: true
   belongs_to :receiver, class_name: 'Company', foreign_key: 'receiver_id', optional: true
-  has_one :message
+  has_one :message, dependent: :destroy
   enum status: [:rejected, :accepted, :pending]
 end
