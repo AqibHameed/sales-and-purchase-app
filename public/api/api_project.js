0d6864e2b4d610e1b518defb6e6ed7411c342c7d
Khuram R <khuramdogar73@gmail.com>
2019-01-21 17:34:08 +0500

    doc updated

--- public/api/api_project.js
+++ public/api/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-21T12:10:23.911Z",
+    "time": "2019-01-21T12:33:44.869Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
