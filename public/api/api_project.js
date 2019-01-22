d969dd9f7fa9759429f8e3e2ece64ec97124df7b
Khuram R <khuramdogar73@gmail.com>
2019-01-22 22:26:30 +0500

    live monitoring api doc updated

--- public/api/api_project.js
+++ public/api/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-22T15:55:34.466Z",
+    "time": "2019-01-22T17:26:03.143Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
