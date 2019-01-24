8b653edc72fb1070982a2a57bc689f78637c2c67
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-01-24 21:40:33 +0500

    api doc

--- public/api/api_project.js
+++ public/api/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-24T12:29:49.220Z",
+    "time": "2019-01-24T16:40:10.242Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
