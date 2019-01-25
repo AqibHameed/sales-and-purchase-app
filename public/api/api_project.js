cd0b3fabd3cefbad9c0a270ff163eb14a6ee3b26
Khuram R <khuramdogar73@gmail.com>
2019-01-25 15:16:51 +0500

    seller score buyer score api doc updated

--- public/api/api_project.js
+++ public/api/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-24T16:40:10.242Z",
+    "time": "2019-01-25T10:16:29.594Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
